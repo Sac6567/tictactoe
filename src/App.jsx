62e90fc8ec626a7e863aaa5c1035eb27ca12c2a9
Sac6567 <sac6567@gmail.com>
2022-08-18 20:52:36 +0530

    Start new game button && Highlight winner

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
-const App = () => {
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isNext: true },
-  ]);
+const NEW_GAME = [{ board: Array(9).fill(null), isNext: true }];
 
+const App = () => {
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
-  const winner = calculateWinner(current.board);
+
+  const { winner, winningSquares } = calculateWinner(current.board);
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -38,11 +38,23 @@ const App = () => {
     setCurrentMove(move);
   };
 
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
   return (
     <div className="app">
       <h1>Tic_Tac_Toe</h1>
       <StatusMessage winner={winner} current={current} />
-      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+      <button type="button" onClick={onNewGame}>
+        Start New Game
+      </button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Square = ({ value, click }) => {
+const Square = ({ value, click, isWinningSquare }) => {
   //console.log('square rerender');
   return (
-    <button type="button" className="square" onClick={click}>
+    <button
+      type="button"
+      className="square"
+      onClick={click}
+      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
+    >
       {value}
     </button>
   );
