6edd73d9b3be4db8acfde1ddc828ca57e9a1f516
Sac6567 <sac6567@gmail.com>
2022-08-18 19:20:50 +0530

    Calculate winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isNext, setIsNext] = useState(false);
-
-  console.log(board);
-  const handleSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
-
-    setBoard(prev => {
-      return prev.map((Square, pos) => {
-        if (pos === position) {
-          return isNext ? 'X' : 'O';
-        }
-        return Square;
-      });
-    });
-    setIsNext(prev => !prev);
-  };
-
+const Board = ({ board, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Square = ({ value, click }) => {
+  //console.log('square rerender');
   return (
     <button type="button" className="square" onClick={click}>
       {value}
